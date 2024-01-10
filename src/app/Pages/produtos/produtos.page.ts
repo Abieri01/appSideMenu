521cc2d287d6dd4ef19dcf532b1b8b88ed9b89cb
Abieri01 <120681169+Abieri01@users.noreply.github.com>
2024-01-10 13:55:53 -0300

    att inicio aula 10/01

--- src/app/Pages/produtos/produtos.page.ts
+++ src/app/Pages/produtos/produtos.page.ts
@@ -1,32 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 import { getStorage, ref, listAll, Storage, getDownloadURL } from '@angular/fire/storage';
-import { collection, getDocs, doc, Firestore } from '@angular/fire/firestore';
-
-
-
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.page.html',
   styleUrls: ['./produtos.page.scss'],
 })
 export class ProdutosPage implements OnInit {
-produto:any = [{
-  nome:'',
-  descricao:'',
-  preco:'',
-  qtd:'',
-  image:''
-}]
+  produtos:any = []
   constructor(private storage:Storage, private firestore:Firestore) { }
-
   ngOnInit() {
     this.listarBanco()
   }
-  async listarBanco(){
-    const querysnapshot = await getDocs(collection(this.firestore, 'Produtos'));
-    querysnapshot.forEach((doc) => {
-      console.log(`$(doc.id) => ${doc.data()['nome']}`);
-      this.produto = [...this.produto, {nome: doc.data()['nome'], descricao: doc.data()['descricao'], preco: doc.data()['preco'], qtd: doc.data()['qtd'], image: doc.data()['image']}]
+  async listarBanco() {
+    const querySnapshot = await getDocs(collection(this.firestore, "Produtos"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()['nome']}`);
+      this.produtos = [...this.produtos, { nome: doc.data()['nome'], descricao: doc.data()['descricao'], preco: doc.data()['preco'], qtd: doc.data()['qtd'], image: doc.data()['image'] }]
     });
   }
+  
 }
